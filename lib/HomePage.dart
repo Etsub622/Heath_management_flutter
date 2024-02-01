95bbf07c457bcaa175461be8df395e343a14a87a
Lelina622 <115922945+Etsub622@users.noreply.github.com>
2024-02-01 11:23:33 -0800

    Update HomePage.dart

--- lib/HomePage.dart
+++ lib/HomePage.dart
@@ -1,45 +1,21 @@
 import 'package:flutter/material.dart';
+import 'package:patient/customAppBar.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: SafeArea(
-        child: Padding(
-          padding: const EdgeInsets.all(25.0),
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [
-              Container(
-                width: 50,
-                height: 50,
-                child: const CircleAvatar(
-                  radius: 50,
-                  backgroundImage: AssetImage('asset/images/logo.jpg'),
-                ),
-              ),
-              Row(
-                children: [
-                  const Icon(
-                    Icons.notifications,
-                    size: 38,
-                    color: Colors.blue,
-                  ),
-                  Container(
-                    width: 35,
-                    height: 35,
-                    child: const CircleAvatar(
-                      radius: 100,
-                      backgroundImage: AssetImage('asset/images/user.jpg'),
-                    ),
-                  ),
-                ],
-              ),
-            ],
+    return const Scaffold(
+      body: Column(
+        children: [
+          CustomAppBar(),
+          Expanded(
+            child: Center(
+              child: Text('home_page'),
+            ),
           ),
-        ),
+        ],
       ),
     );
   }
