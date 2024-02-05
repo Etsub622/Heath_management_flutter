916a11e0f0e3212da15d46ef5499aebfdad4c53f
Lelina622 <115922945+Etsub622@users.noreply.github.com>
2024-02-05 06:15:57 -0800

    Update Services.dart

--- lib/Services.dart
+++ lib/Services.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:patient/HouseRequest.dart';
+import 'package:patient/NotificationPage.dart';
+import 'package:patient/Settings.dart';
 import 'package:patient/customAppBar.dart';
 
 class Services extends StatelessWidget {
@@ -6,14 +9,61 @@ class Services extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    return Scaffold(
       body: Column(
         children: [
-          CustomAppBar(),
-          Expanded(
-            child: Center(
-              child: Text('Servise_page'),
+          const CustomAppBar(),
+          Padding(
+            padding: const EdgeInsets.all(18.0),
+            child: TextFormField(
+              decoration: InputDecoration(
+                  prefixIcon: InkWell(
+                    onTap: () {},
+                    child: const Padding(
+                      padding: EdgeInsets.only(left: 25.0, right: 10),
+                      child:
+                          Icon(Icons.search, color: Colors.lightBlue, size: 35),
+                    ),
+                  ),
+                  filled: true,
+                  fillColor: Colors.white,
+                  contentPadding: const EdgeInsets.all(20),
+                  border: const OutlineInputBorder(
+                    borderRadius: BorderRadius.all(Radius.circular(40)),
+                    borderSide: BorderSide.none,
+                  ),
+                  enabledBorder: const OutlineInputBorder(
+                    borderRadius: BorderRadius.all(Radius.circular(40)),
+                    borderSide: BorderSide(color: Colors.black26),
+                  ),
+                  hintText: 'Search for transport ...',
+                  hintStyle:
+                      const TextStyle(fontSize: 18, color: Colors.black26)),
             ),
+          ),
+          GestureDetector(
+            onTap: () {
+              Navigator.of(context).push(MaterialPageRoute(builder: (context) {
+                return HouseRequest();
+              }));
+            },
+            child: const Text('house visit request'),
+          ),
+          GestureDetector(
+            onTap: () {
+              Navigator.of(context).push(MaterialPageRoute(builder: (context) {
+                return NotificationPage();
+              }));
+            },
+            child: const Text('notifications'),
+          ),
+          GestureDetector(
+            onTap: () {
+              Navigator.of(context).push(MaterialPageRoute(builder: (context) {
+                return SettingPage();
+              }));
+            },
+            child: const Text('settings'),
           ),
         ],
       ),
